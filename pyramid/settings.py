3cb061e0e3e26b2d3ac579a8a19cc05330db2de5
Asah Lesley <76005372+leoasah2047@users.noreply.github.com>
2023-07-11 14:01:44 +0100

    Update settings.py

--- pyramid/settings.py
+++ pyramid/settings.py
@@ -83,12 +83,6 @@ WSGI_APPLICATION = "pyramid.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -98,19 +92,6 @@ DATABASES = {
         'HOST': 'pyramidlive-server.postgres.database.azure.com',
     }
 }
-
-
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.mysql",
-#        "OPTIONS": {'charset': 'utf8mb4', 'use_unicode': True, },       
-#        "NAME": "pyramidindev",
-#        "USER": "pyramidindev",
-#        "PASSWORD": "Universe&007",
-#        "HOST": "pyramidindev.cumel6cv7bwg.us-east-2.rds.amazonaws.com",
-#        "PORT": "3306",
-#    }
-#}
 
 
 # Password validation
