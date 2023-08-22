2db3aadb7c4b381888a030655c52a611ebd69270
leoasah2047 <leoasah2047@gmail.com>
2023-08-23 00:07:16 +0100

    lop

--- portfolio/signals.py
+++ portfolio/signals.py
@@ -1,18 +1,3 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from .models import User
-from portfolio.models import FiatWallet, CryptoWallet
-
-
-@receiver(post_save, sender=User)
-def post_save_user_wallet(sender, **kwargs):
-	user = sender
-    fiat = FiatWallet.objects.create(user=user)
-    fiat.save()
-
-
-@receiver(post_save, sender=User)
-def create_wallet(sender, instance, created, **kwargs):
-    if created:
-        CryptoWallet.objects.create(user=instance)
