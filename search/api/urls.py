8420e3835bb73297cdff689b64acb143d64e7c13
Asah Lesley <76005372+leoasah2047@users.noreply.github.com>
2023-08-23 14:15:28 +0100

    Update urls.py

--- search/api/urls.py
+++ search/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetBusinessNews, FeaturedBlocks, RecommendedUsers, FeaturedUsers, SearchLatest, GetCategoryNews, GetStockNews, GetCryptoNews, GetStockQuoteData, GetCompanySecondProfile, GetCryptoProfileBySymbol, GetCompanyFinancials, GetCompanyProfile, SearchStocks, GetStocksByThemes, GetCryptoPriceData, GetStockPriceData, GetAllCryptoAssets, GetAllStocks, GetLogo, GetClock, GetAllCryptoAssets, GetAsset, SearchUsers, SearchBlocks, SearchPosts, BlockCategory, ListAllPosts
+from .views import GetBusinessNews, FeaturedBlocks, RecommendedUsers, FeaturedUsers, SearchLatest, GetCategoryNews, GetStockNews, GetCryptoNews, GetStockQuoteData, GetCompanySecondProfile, GetCryptoProfileBySymbol, GetCompanyFinancials, GetCompanyProfile, SearchStocks, GetStocksByThemes, GetCryptoPriceData, GetStockPriceData, SearchUsers, SearchBlocks, SearchPosts, BlockCategory, ListAllPosts
 
 urlpatterns = [
     #SPOTLIGHT
@@ -17,12 +17,9 @@ urlpatterns = [
     #POSTS
     path('posts/all', ListAllPosts.as_view(), name='townsquare'),
     path('posts', SearchPosts.as_view(), name='posts'),
-    #ASSETS
-    path('get-asset', GetAsset.as_view(), name='get-asset'),
     #CRYPTO
     path('get-crypto-profile-by-symbol', GetCryptoProfileBySymbol.as_view(), name='get-crypto-profile-by-symbol'),
     path('get-crypto-price-data', GetCryptoPriceData.as_view(), name='get-crypto-price-data'),
-    path('all-crypto', GetAllCryptoAssets.as_view(), name='all-crypto'),
     #STOCKS
     path('get-company-financials', GetCompanyFinancials.as_view(), name='get-company-financials'),
     path('get-company-profile', GetCompanyProfile.as_view(), name='get-company-profile'),
@@ -31,14 +28,9 @@ urlpatterns = [
     path('get-stocks-by-theme', GetStocksByThemes.as_view(), name='get-stocks-by-theme'),
     path('get-stock-quote-data', GetStockQuoteData.as_view(), name='get-stock-quote-data'),
     path('get-stock-price-data', GetStockPriceData.as_view(), name='get-stock-price-data'),
-    path('all-stocks', GetAllStocks.as_view(), name='all-stocks'),
     #NEWS
     path('get-crypto-news', GetCryptoNews.as_view(), name='get-crypto-news'),
     path('get-stock-news', GetStockNews.as_view(), name='get-stock-news'),
     path('get-business-news', GetBusinessNews.as_view(), name='get-business-news'),
-    path('get-category-news/<str:category>/', GetCategoryNews.as_view(), name='get-category-news'),
-    #LOGO
-    path('get-logo', GetLogo.as_view(), name='get-logo'),
-    #CLOCK
-    path('get-clock', GetClock.as_view(), name='get-clock'),
+    path('get-category-news/<str:category>/', GetCategoryNews.as_view(), name='get-category-news')
 ]
