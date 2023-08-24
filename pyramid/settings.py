a15c44d1af0976e71b7d7e413dbdd02df60e16e7
Asah Lesley <76005372+leoasah2047@users.noreply.github.com>
2023-08-24 11:18:27 +0100

    Update settings.py

--- pyramid/settings.py
+++ pyramid/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-1yd$y6)cjv&o8p&-fl&alj=n+s6!tu7)z(160ld%ml%m36qax!"
 
