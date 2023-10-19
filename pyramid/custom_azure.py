ba1b5bbafc7c578c4015fb39834071c2c670ca47
Asah Lesley <76005372+leoasah2047@users.noreply.github.com>
2023-10-19 08:54:04 +0100

    Update custom_azure.py

--- pyramid/custom_azure.py
+++ pyramid/custom_azure.py
@@ -1,13 +1,13 @@
 from storages.backends.azure_storage import AzureStorage
 # Azure configuration secret key --- GG6bWpYonhrHekFgSDqEq7T86q8yCEEB4q/gt0hSwQ3WH8AW
 class AzureMediaStorage(AzureStorage):
-    account_name = 'pyramidtestingstorage' # Must be replaced by your <storage_account_name>
-    account_key = '3il2ezW/jYtaUXlb9wDqhxr6R+SVSdGxDJJ4AnDazq5bQcrSWLJwzKcaWXbz3NZLlTwdxbbEtE7l+ASt/GaZ8A==' # Must be replaced by your <storage_account_key>
+    account_name = 'monolithteststorage' # Must be replaced by your <storage_account_name>
+    account_key = '3mROEAnuUtkPJfAp1uNt0p9mDjpfHnp7Din89fb3hwy0az2HChMiKwK1dwDzYDRcNQgkDgzm5ljA+AStMvJsVw==' # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'pyramidtestingstorage' # Must be replaced by your storage_account_name
-    account_key = '3il2ezW/jYtaUXlb9wDqhxr6R+SVSdGxDJJ4AnDazq5bQcrSWLJwzKcaWXbz3NZLlTwdxbbEtE7l+ASt/GaZ8A==' # Must be replaced by your <storage_account_key>
+    account_name = 'monolithteststorage' # Must be replaced by your storage_account_name
+    account_key = '3mROEAnuUtkPJfAp1uNt0p9mDjpfHnp7Din89fb3hwy0az2HChMiKwK1dwDzYDRcNQgkDgzm5ljA+AStMvJsVw==' # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
